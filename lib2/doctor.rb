b535bc56530bfb01debc9a01d147127745763505
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-05 10:56:43 +0000

    automatically backed up by learn

--- lib2/doctor.rb
+++ lib2/doctor.rb
@@ -17,26 +17,15 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self }
+    Appointment.all.select { |a| a.doctor == self }
   end
   
   def patients
-    appointments.map { |appointment| appointment.patient }
+    appointments.map { |a| a.patient }
   end
 end 
 
 # class Artist 
-#   attr_accessor :name
-#   @@all = []
-  
-#   def self.all 
-#     @@all
-#   end
-  
-#   def initialize(name)
-#     @name = name 
-#     @@all << self
-#   end
   
 #   def new_song(name, genre)
 #     Song.new(name, self, genre)
