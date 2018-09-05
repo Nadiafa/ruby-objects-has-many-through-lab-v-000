c770fda7081e6e3a2e63f8a1473612d0627b4465
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-05 11:10:21 +0000

    automatically backed up by learn

--- lib2/doctor.rb
+++ lib2/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
     @@all << self
   end
   
-  def new_appointment(doctor, date)
+  def new_appointment(patient, date)
     Appointment.new_appointment(self, date)
   end
   
@@ -26,18 +26,14 @@ class Doctor
 end 
 
 
-class Doctor
-  
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
-  end
-
-  def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self}
-  end
-
-  def patients
-    appointments.map{|appointment| appointment.patient}
-  end
-end
+# class Doctor
+#   def new_appointment(patient, date)
+#     Appointment.new(patient, self, date)
+#   end
+#   def appointments
+#     Appointment.all.select { |appointment| appointment.doctor == self}
+#   end
+#   def patients
+#     appointments.map{|appointment| appointment.patient}
+#   end
+# end
