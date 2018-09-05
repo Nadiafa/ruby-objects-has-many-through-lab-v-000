e85d4bdcbd52cebced6f9da9b50dbadfa57b312b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-05 10:45:38 +0000

    automatically backed up by learn

--- lib2/patient.rb
+++ lib2/patient.rb
@@ -1,9 +1,10 @@
 # rspec spec/06_patient_spec.rb
 class Patient 
   attr_accessor :name 
-  
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end 
   
   def new_appointment(doctor, date)
