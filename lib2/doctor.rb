b5a86c0075d5af89a67b6dd89bad7ee7bba750b3
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-05 10:55:30 +0000

    automatically backed up by learn

--- lib2/doctor.rb
+++ lib2/doctor.rb
@@ -24,3 +24,30 @@ class Doctor
     appointments.map { |appointment| appointment.patient }
   end
 end 
+
+# class Artist 
+#   attr_accessor :name
+#   @@all = []
+  
+#   def self.all 
+#     @@all
+#   end
+  
+#   def initialize(name)
+#     @name = name 
+#     @@all << self
+#   end
+  
+#   def new_song(name, genre)
+#     Song.new(name, self, genre)
+#   end
+
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#   end
+
+#   def genres
+#     songs.map { |song| song.genre  }
+#   end
+  
+# end
